26a4e8f5e642119b6012057e684e7c5b0bc35a29
fzampirolli <fzampirolli@gmail.com>
2020-12-24 05:36:12 -0300

    new version 5.2

---
+++ static/colab/mctest.py
@@ -0,0 +1,8 @@
+def drawCircles():
+    str1 = '''
+% drawCircles
+\\vspace{-5mm}
+\\leavevmode\\put(-11,0){\\color{black}\\circle*{15}}\\hspace{-0mm}
+\\leavevmode\\put(509,0){\\color{black}\\circle*{15}}
+        '''
+    return str1
