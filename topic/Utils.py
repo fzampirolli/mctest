2a4e8557bbdef66b0a4301d4e8b01032aea765ff
fzampirolli <fzampirolli@gmail.com>
2021-11-07 14:57:03 -0300

    Topic - create_PDF

--- topic/Utils.py
+++ topic/Utils.py
@@ -36,7 +36,6 @@ def PT_BR(my_value):
 
 def drawMatrix(A, myfile):
     """
-
     :param A: matrix
     :param myfile: name of the file defined in [[def: ... ]]
     :return: file in server
@@ -69,38 +68,52 @@ def drawMatrix(A, myfile):
     plt.close()
 
 
-def printMatrix(A, title='', left='[', right=']'):
+def printMatrix(A, title='', left='[', right=']', align='r', dec='3.2f'):
     """
       Draw a matrix
-      :param A: matrix numpy
+      :param A: matrix = 2D list
       :param title: title, ex.: title = [...]
       :param left: left symbol
       :param right: right symbol
+      :param align: aling in r, l, c, C, S
+      :param dec: format of decimals
       :return: string of draw matrix
     """
-    A = np.array(A)
     str1 = """
 \\setcounter{MaxMatrixCols}{__size__}
 \\begin{displaymath}
 \\textbf{__title__}
 \\left__tleft__
-\\begin{matrix}
+\\begin{matrix*}[__align__]
 """
+    import itertools
 
-    for i in range(A.shape[0]):
-        for j in range(A.shape[1]):
-            str1 += str(A[i, j])
-            if j < A.shape[1] - 1:
-                str1 += "&"
-        if i < A.shape[0] - 1:
+    # A = np.array(A)
+    B = list(itertools.chain.from_iterable(A))  # 2D to 1D
+
+    if all(isinstance(x, int) for x in B):
+        dig = 1 if min(B) < 0 else 0
+        digitos = '%' + str(len(str(max(B))) + dig) + 'd '
+    elif all(isinstance(x, float) for x in B):
+        digitos = '%' + dec
+    else:
+        digitos = '%s'
+
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            str1 += digitos % A[i][j]
+            if j < len(A[0]) - 1:
+                str1 += "& "
+        if i < len(A) - 1:
             str1 += "\\\\"
         str1 += "\n"
 
-    str1 += """\\end{matrix}
+    str1 += """\\end{matrix*}
 \\right__tright__
-\\end{displaymath}
-    """
+\\end{displaymath}"""
+
     str1 = str1.replace('__tleft__', left).replace('__tright__', right)
     str1 = str1.replace('__title__', title)
+    str1 = str1.replace('__align__', align)
     str1 = str1.replace('__size__', str(len(A[0])))
     return str1
