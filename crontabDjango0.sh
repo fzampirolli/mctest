dcbd7652fb03c5d76a2f409248c61e85dcb3e781
fz <fzampirolli@gmail.com>
2024-11-13 16:17:22 -0300

    mctest-validator-api.zip

--- crontabDjango0.sh
+++ crontabDjango0.sh
@@ -5,6 +5,24 @@ if pgrep -x "python3" > /dev/null; then
     echo "O processo Python3 está em execução. Não iniciando um novo processo."
 else
     echo "O processo Python3 não está em execução. Iniciando um novo processo."
-
     . /home/operador/PycharmProjects/crontabDjango.sh
+fi
+
+# Verificar se a API Go está rodando na porta 8001
+if sudo lsof -i :8001 | grep LISTEN > /dev/null; then
+    echo "A API Go já está em execução na porta 8001."
+else
+    echo "A API Go não está em execução. Iniciando a API Go na porta 8001."
+
+    # Salvar o diretório atual
+    CURRENT_DIR=$(pwd)
+
+    # Navegar para o diretório do mctest-validator-api
+    cd /home/operador/PycharmProjects/mctest/mctest-validator-api || exit
+
+    # Executar o main.go e redirecionar a saída para logfile.txt
+    go run main.go > logfile.txt 2>&1 &
+
+    # Voltar ao diretório original
+    cd "$CURRENT_DIR"
 fi
--- topic/UtilsMCTest4.py
+++ topic/UtilsMCTest4.py
@@ -195,6 +195,7 @@ class UtilsMC(object):
         if myDef is not None:
             import requests
             url = f'http://localhost:8080/mctest/validator/question/{pk}'
+            url = f'http://mctest.ufabc.edu.br:8001/mctest/validator/question/{pk}'
             files = {'file': '\n'.join(myDef)}
             r = requests.post(url, files=files)
             res = r.json()
